426809dae7aed0ddd15420156779ad51351f5909
xuxy <xuxiaoyu2010@gmail.com>
2022-11-29 11:16:46 +0800

    fix build sql

--- build/sql/blog.sql
+++ build/sql/blog.sql
@@ -9,64 +9,44 @@ $$ language 'plpgsql';
 
 
 CREATE TYPE user_role AS ENUM('admin', 'general');
+CREATE TYPE public.gender AS ENUM ('unknown', 'female', 'male');
+COMMENT ON TYPE public.gender IS '性别';
 
 -- Table: public.user
 
 -- DROP TABLE IF EXISTS public."user";
 
-CREATE TABLE IF NOT EXISTS public."user"
-(
-    id bigint NOT NULL DEFAULT nextval('user_id_seq'::regclass),
-    username character varying(16) COLLATE pg_catalog."default" NOT NULL,
-    email character varying(64) COLLATE pg_catalog."default",
-    nickname character varying COLLATE pg_catalog."default" NOT NULL,
-    create_time timestamp(0) with time zone NOT NULL DEFAULT now(),
-    update_time timestamp(0) with time zone NOT NULL DEFAULT now(),
-    role user_role NOT NULL DEFAULT 'general'::user_role,
-    password character varying(128) COLLATE pg_catalog."default",
-    gender gender NOT NULL DEFAULT 'unknown'::gender,
-    CONSTRAINT user_pk PRIMARY KEY (id),
-    CONSTRAINT user_name_unique_idx UNIQUE (username)
-)
+CREATE TABLE public."user" (
+	id bigserial NOT NULL, -- 用户id
+	username varchar(16) NOT NULL,
+	email varchar(64) NULL, -- 邮箱
+	nickname varchar NOT NULL,
+	"role" public.user_role NOT NULL DEFAULT 'general'::user_role, -- 用户角色
+	gender public.gender NOT NULL DEFAULT 'unknown'::gender, -- 性別
+	"password" varchar(128) NULL,
+	create_time timestamptz(0) NOT NULL DEFAULT now(), -- 创建时间
+	update_time timestamptz(0) NOT NULL DEFAULT now(), -- 修改时间        
+	CONSTRAINT user_name_unique_idx UNIQUE (username),
+	CONSTRAINT user_pk PRIMARY KEY (id)
+);
+COMMENT ON TABLE public."user" IS '用户表，包含管理用户和普通用户';
 
-TABLESPACE pg_default;
+-- Column comments
 
-ALTER TABLE IF EXISTS public."user"
-    OWNER to postgres;
+COMMENT ON COLUMN public."user".id IS '用户id';
+COMMENT ON COLUMN public."user".email IS '邮箱';
+COMMENT ON COLUMN public."user".create_time IS '创建时间';
+COMMENT ON COLUMN public."user".update_time IS '修改时间';
+COMMENT ON COLUMN public."user"."role" IS '用户角色';
+COMMENT ON COLUMN public."user".gender IS '性別';
 
-COMMENT ON TABLE public."user"
-    IS '用户表，包含管理用户和普通用户';
+-- Constraint comments
 
-COMMENT ON COLUMN public."user".id
-    IS '用户id';
+COMMENT ON CONSTRAINT user_name_unique_idx ON public."user" IS 'username needs unique';
 
-COMMENT ON COLUMN public."user".email
-    IS '邮箱';
+-- Table Triggers
 
-COMMENT ON COLUMN public."user".create_time
-    IS '创建时间';
-
-COMMENT ON COLUMN public."user".update_time
-    IS '修改时间';
-
-COMMENT ON COLUMN public."user".role
-    IS '用户角色';
-
-COMMENT ON COLUMN public."user".password
-    IS 'password for login';
-
-COMMENT ON COLUMN public."user".gender
-    IS '性別';
-
-COMMENT ON CONSTRAINT user_name_unique_idx ON public."user"
-    IS 'username needs unique';
-
--- Trigger: update_user_update_time
-
--- DROP TRIGGER IF EXISTS update_user_update_time ON public."user";
-
-CREATE TRIGGER update_user_update_time
-    BEFORE UPDATE 
-    ON public."user"
-    FOR EACH ROW
-    EXECUTE FUNCTION public.update_modified_column();
+create trigger update_user_update_time before
+update
+    on
+    public."user" for each row execute function update_modified_column();
