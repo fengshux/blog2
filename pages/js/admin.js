2125cd170b3feee153da9ac0455a9e9faee75706
xuxy <xuxiaoyu2010@gmail.com>
2022-12-14 10:11:51 +0800

    admin

--- pages/js/admin.js
+++ pages/js/admin.js
@@ -40,6 +40,7 @@
         },
     });
     
+    
     function renderSettings(data) {
         for (var v of data) {
             switch( v["key"] ) {
@@ -57,11 +58,28 @@
             for (var d of data.list) {
                 html += `<tr><td>${d.username}</td><td>${d.nickname}</td>`
                     +`<td>${d.email}</td><td>${d.gender}</td><td>${d.role}</td>`
-                    +`<td><button type="button" class="btn btn-sm btn-primary">修改密码</button>`
-                    +`<button type="button" class="btn btn-sm btn-danger">删除</button></td></tr>`;
+                    +`<td><button type="button" class="btn btn-sm btn-primary op-btn" value="${d.id}">修改密码</button>`
+                    +`<button type="button" class="btn btn-sm btn-danger op-btn" value="${d.id}">删除</button></td></tr>`;
             }
             $("#user-list-table").append(html);
         }
+
+
+        // add click 
+        $("button.op-btn").on("click", function() {
+
+            if ($(this).text() == "修改密码")  {
+                // TODO 修改密码
+                
+            } else if ($(this).text() == "删除") {
+                if (confirm("删除后不可恢复，确定删除吗？")) {
+                    // TODO 删除用户
+                }
+            }
+            
+            
+        });
+
     }
 
     
